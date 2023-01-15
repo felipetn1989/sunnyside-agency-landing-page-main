7be4058effda9e7c56e055dc5f1da49840580907
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-15 17:30:46 -0300

    menu

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,8 @@ hamburgerIcon.addEventListener("click", menu);
 let navMenu = document.querySelector(".nav__top");
 
 function menu() {
-  if (window.getComputedStyle(navMenu).display == 'none') {
-    navMenu.style.display = "block";
+  if (window.getComputedStyle(navMenu).display == "none") {
+    navMenu.style.display = "flex";
   } else {
     navMenu.style.display = "none";
   }
