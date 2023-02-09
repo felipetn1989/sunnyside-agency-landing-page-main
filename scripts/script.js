9bd14befcc489d58300b9b9720739745edf616e1
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-02-09 06:16:23 -0300

    code update

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,5 @@ hamburgerIcon.addEventListener("click", menu);
 let navMenu = document.querySelector(".nav__top");
 
 function menu() {
-  if (window.getComputedStyle(navMenu).display == "none") {
-    navMenu.style.display = "flex";
-  } else {
-    navMenu.style.display = "none";
-  }
+  navMenu.classList.toggle("menuDisplay")
 }
